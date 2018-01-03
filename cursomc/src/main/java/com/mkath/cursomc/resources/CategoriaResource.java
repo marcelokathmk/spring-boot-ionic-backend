2e3c019ccd2523ff37ae9044d63e5ff094b06c2e
Marcelo Kath <marcelokathmk@gmail.com>
2018-01-03 15:43:08 -0200

    Criação de classe de dominio

--- cursomc/src/main/java/com/mkath/cursomc/resources/CategoriaResource.java
+++ cursomc/src/main/java/com/mkath/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.mkath.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mkath.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Ok";
+	public List<Categoria> listar() {
+		Categoria c1 = new Categoria(1, "Informática");
+		Categoria c2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> listCategoria = new ArrayList<>();
+		listCategoria.add(c1);
+		listCategoria.add(c2);
+		
+		return listCategoria;
 	}
 }
