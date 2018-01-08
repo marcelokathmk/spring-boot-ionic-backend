ba71a31c30ed6a63c2133b3bb68ff12e7af5fa7a
Marcelo Kath <marcelokathmk@gmail.com>
2018-01-08 09:43:28 -0200

    Inserindo um novo cliente com POST

--- cursomc/src/main/java/com/mkath/cursomc/resources/ClienteResource.java
+++ cursomc/src/main/java/com/mkath/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.mkath.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.mkath.cursomc.domain.Cliente;
 import com.mkath.cursomc.dto.ClienteDTO;
+import com.mkath.cursomc.dto.ClienteNewDTO;
 import com.mkath.cursomc.services.ClienteService;
 
 @RestController
@@ -61,5 +64,13 @@ public class ClienteResource {
 		Page<Cliente> pageCat = service.findPage(page, linesPerPage, orderBy, direction);
 		Page<ClienteDTO> pageDto = pageCat.map(obj -> new ClienteDTO(obj));
 		return ResponseEntity.ok().body(pageDto);
+	}
+	
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> inserir(@Valid @RequestBody ClienteNewDTO objDTO){
+		Cliente obj = service.fromDTO(objDTO);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 }
--- cursomc/src/main/java/com/mkath/cursomc/services/ClienteService.java
+++ cursomc/src/main/java/com/mkath/cursomc/services/ClienteService.java
@@ -8,9 +8,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+
+import com.mkath.cursomc.domain.Cidade;
 import com.mkath.cursomc.domain.Cliente;
+import com.mkath.cursomc.domain.Endereco;
+import com.mkath.cursomc.domain.enums.TipoCliente;
 import com.mkath.cursomc.dto.ClienteDTO;
+import com.mkath.cursomc.dto.ClienteNewDTO;
+import com.mkath.cursomc.repositories.CidadeRespository;
 import com.mkath.cursomc.repositories.ClienteRepository;
+import com.mkath.cursomc.repositories.EnderecoRepository;
 import com.mkath.cursomc.services.exception.DataIntegretyException;
 import com.mkath.cursomc.services.exception.ObjectNotFoundException;
 
@@ -19,6 +26,12 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repository;
+	
+	@Autowired
+	private CidadeRespository cidadeRespository;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 	
 	public Cliente find(Integer id) {
 		Cliente obj = repository.findOne(id);
@@ -58,7 +71,29 @@ public class ClienteService {
 		return repository.findAll(pageRequest);
 	}
 	
-	public Cliente fromDTO(ClienteDTO clienteDTO) {
-		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	public Cliente fromDTO(ClienteDTO objDTO) {
+		return new Cliente(objDTO.getId(), objDTO.getNome(), objDTO.getEmail(), null, null);
+	}
+
+	public Cliente fromDTO(ClienteNewDTO objDTO) {
+		Cliente cli = new Cliente(null, objDTO.getNome(), objDTO.getEmail(), objDTO.getCpfOuCnpj(), TipoCliente.toEnum(objDTO.getTipo()));
+		Cidade cid = cidadeRespository.findOne(objDTO.getCidadeId());
+		Endereco end = new Endereco(null, objDTO.getLogradouro(), objDTO.getNumero(), objDTO.getComplemento(), objDTO.getBairro(), objDTO.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDTO.getTelefone1());
+		if	(objDTO.getTelefone2() != null) {
+			cli.getTelefones().add(objDTO.getTelefone2());
+		}
+		if	(objDTO.getTelefone3() != null) {
+			cli.getTelefones().add(objDTO.getTelefone3());
+		}
+		return cli;
+	}
+
+	public Cliente insert(Cliente obj) {
+		obj.setId(null);
+		obj = repository.save(obj);
+		enderecoRepository.save(obj.getEnderecos());
+		return obj;
 	}
 }
