f7fd221fa2631d762808235b80e2ab2e47edba8a
Marcelo Kath <marcelokathmk@gmail.com>
2018-01-25 09:16:33 -0200

    Salvar arquivos no S3

--- src/main/java/com/mkath/cursomc/CursomcApplication.java
+++ src/main/java/com/mkath/cursomc/CursomcApplication.java
@@ -1,18 +1,24 @@
 package com.mkath.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mkath.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		
+		s3Service.uploadFile("D:\\Users\\TECBMMKH\\Downloads\\credentials.csv");
 	}
 }
