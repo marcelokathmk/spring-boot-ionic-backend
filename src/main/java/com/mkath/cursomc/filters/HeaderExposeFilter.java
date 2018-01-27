c6b7a00f1d266262c6e276480225766880b74a46
Marcelo Kath <marcelokathmk@gmail.com>
2018-01-27 13:55:28 -0200

    Expondo location no cabecalho da request

---
+++ src/main/java/com/mkath/cursomc/filters/HeaderExposeFilter.java
@@ -0,0 +1,35 @@
+package com.mkath.cursomc.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HeaderExposeFilter implements Filter{
+
+	@Override
+	public void destroy() {
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletResponse resp = (HttpServletResponse) response;
+		resp.addHeader("access-control-expose-controls", "location");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		
+	}
+
+}
