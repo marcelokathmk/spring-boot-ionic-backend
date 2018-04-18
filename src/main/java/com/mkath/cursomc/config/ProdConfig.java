02583e4fdf77c1788e7061d85d90c5cb551ccf90
Marcelo Kath <marcelokathmk@gmail.com>
2018-04-18 09:53:17 -0300

    Ajuste finais para envio para produção

---
+++ src/main/java/com/mkath/cursomc/config/ProdConfig.java
@@ -0,0 +1,35 @@
+package com.mkath.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.mkath.cursomc.services.DBService;
+import com.mkath.cursomc.services.EmailService;
+import com.mkath.cursomc.services.SmtpEmailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+
+	@Autowired
+	private DBService dbservice;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDevDataBase() {
+		if	("create".equals(strategy)) {
+			dbservice.instantiateDataBase();
+		}
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpEmailService();
+	}
+}
